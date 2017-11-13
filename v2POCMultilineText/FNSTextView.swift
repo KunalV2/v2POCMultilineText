638c42a5b4e157cc7f8c4f8ad2840e88ff2e73d4
KunalV2 <kunal.parekh@v2solutions.com>
2017-11-13 11:14:39 +0530

    Made some modification in FNSTextView

--- v2POCMultilineText/FNSTextView.swift
+++ v2POCMultilineText/FNSTextView.swift
@@ -11,7 +11,8 @@ import UIKit
 class FNSTextView: UITextView {
     
     private var modifiedText: String = ""
-    private var madeTruncateString = false
+    var isTextModified = false
+    var isShrink = false
     private var glyphRangeArray = [NSRange]()
     private var lineRectArray = [CGRect]()
     private var usedRectArray = [CGRect]()
@@ -19,69 +20,102 @@ class FNSTextView: UITextView {
     override var text: String! {
 
         didSet {
-            
-            if !madeTruncateString{
-                
+            if !isTextModified && isShrink{
                 modifiedText = self.text
                 glyphRangeArray.removeAll()
                 self.layoutManager.enumerateLineFragments(forGlyphRange: NSMakeRange(0, layoutManager.numberOfGlyphs), using: {
                     (lineRect: CGRect, usedRect: CGRect, textContainer: NSTextContainer!, glyphRange: NSRange, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
-                    
                     self.glyphRangeArray.append(glyphRange)
                     self.lineRectArray.append(lineRect)
                     self.usedRectArray.append(usedRect)
-                    
-                    print("Line rect \(lineRect)")
-                    print("Used rect \(usedRect)")
-                    print("First line \((self.text as NSString).substring(with: glyphRange))")
-                    print("Range : \(glyphRange.location) , \(glyphRange.length)")
                 })
                 
-                if glyphRangeArray.count > 2 && self.frame.size.height <= 58{
+                // 
+                
+                if glyphRangeArray.count > 2 {
                     
                     let secondGlyphRange = glyphRangeArray[1]
                     
                     if (self.text as NSString).substring(with: secondGlyphRange) == "\n"{
+                        
                         let firstGlyphRange = glyphRangeArray[0]
                         let firstLineRect = lineRectArray[0]
-                        let firstUsedRect = usedRectArray[0]
+                        let firstUsedRect = usedRectArray[0]                        
+                        
+                        var subString = (self.text as NSString).substring(with: firstGlyphRange)
+                        subString = subString.trimmingCharacters(in: .whitespacesAndNewlines)
                         
                         if (firstLineRect.size.width - firstUsedRect.size.width) > 30{
-                            
-                            var subString = (self.text as NSString).substring(with: firstGlyphRange)
-                            subString = subString.trimmingCharacters(in: .whitespacesAndNewlines)
-                            var appendString2 = subString+"..."
-                            
-                            modifiedText = modifiedText.replacingOccurrences(of: subString, with: appendString2)
-                            
-                           //  modifiedText = modifiedText.stringByReplacingFirstOccurrenceOfString(target:subString , withString: appendString2)
-                            madeTruncateString = true
+                           // var appendString2 = subString+"..."
+                            modifiedText = modifiedText.replacingOccurrences(of: subString, with: subString+"...")
+                            isTextModified = true
                             self.text = modifiedText
                         }
                         else{
-                            
+                            let subNSString = subString as NSString
+                            let replaceString = subNSString.substring(to: subNSString.length - 3)
+                            modifiedText = modifiedText.replacingOccurrences(of: subString, with: replaceString+"...")
+                            isTextModified = true
+                            self.text = modifiedText
                         }
                     }
                     else{
                      
-                        let secondGlyphRange = glyphRangeArray[1]
+                        var secondGlyphRange = glyphRangeArray[1]
                         let secondLineRect = lineRectArray[1]
                         let secondUsedRect = usedRectArray[1]
+                        var subString = (self.text as NSString).substring(with: secondGlyphRange)
+                        var isStringContainsNewLine = false
+                        
+                        if subString.contains("\n"){
+                            isStringContainsNewLine = true
+                        }
+                        
+                        let sNSString = subString as NSString
+                        let lastString = sNSString.substring(to: sNSString.length - 1)
+                        
+                        print("Last string:- \(lastString)")
+                        
+                        subString = subString.trimmingCharacters(in: .whitespacesAndNewlines)
                         
                         if (secondLineRect.size.width - secondUsedRect.size.width) > 30{
+//                            var appendString2 = subString+"..."
                             
-                            var subString = (self.text as NSString).substring(with: secondGlyphRange)
-                            subString = subString.trimmingCharacters(in: .whitespacesAndNewlines)
-                            var appendString2 = subString+"..."
+                        //    let subNSString = subString as NSString
+                           // var replaceString = subNSString.substring(to: subNSString.length - 3)
+                            secondGlyphRange.length = secondGlyphRange.location + 1
+                          //  var replaceString = subString+"...           "
                             
-                            modifiedText = modifiedText.replacingOccurrences(of: subString, with: appendString2)
-                            
-                            //  modifiedText = modifiedText.stringByReplacingFirstOccurrenceOfString(target:subString , withString: appendString2)
-                            madeTruncateString = true
+                            if isStringContainsNewLine{
+                                modifiedText = (modifiedText as NSString).replacingCharacters(in: secondGlyphRange, with: subString+"... \n")
+                                
+                            }
+                            else{
+                                modifiedText = (modifiedText as NSString).replacingCharacters(in: secondGlyphRange, with: subString+"...           ")
+                            }
+
+                            isTextModified = true
                             self.text = modifiedText
                         }
                         else{
+                            let subNSString = subString as NSString
+                            var replaceString = subNSString.substring(to: subNSString.length - 3)
+                            secondGlyphRange.length = secondGlyphRange.location + 1
                             
+                            
+                            replaceString = replaceString+"... "
+                            
+                            
+                            if isStringContainsNewLine{
+                                modifiedText = (modifiedText as NSString).replacingCharacters(in: secondGlyphRange, with: subString+"... \n")
+                                
+                            }
+                            else{
+                                modifiedText = (modifiedText as NSString).replacingCharacters(in: secondGlyphRange, with: replaceString)
+                            }
+                            
+                            isTextModified = true
+                            self.text = modifiedText
                         }
                         
                     }
--- v2POCMultilineText/ViewController.swift
+++ v2POCMultilineText/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textViewMultiline: UITextView!
+    @IBOutlet weak var textViewMultiline: FNSTextView!
     @IBOutlet weak var labelMultiline: UILabel!
+    @IBOutlet weak var textViewHeightConstraint: NSLayoutConstraint!
+    
+    var selctedTextType = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +22,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    @IBAction func textCClicked(_ sender: Any) {
+        self.setMultiLineTextWithNewLinesAndLink()
+    }
+    
+    @IBAction func textBClicked(_ sender: Any) {
+        self.setMultilineTextA()
+    }
+    
+    @IBAction func textAClicked(_ sender: Any) {
+        self.setMultiLineText()
+    }
+    
     func intializeTextViewAndLabel(){
         
         // TextView intialization
         
-        self.textViewMultiline.textContainer.maximumNumberOfLines = 0
+        //self.textViewMultiline.textContainer.maximumNumberOfLines = 0
         self.textViewMultiline.dataDetectorTypes = .link
+        self.textViewMultiline.isEditable = false
+      //  self.textViewMultiline.isSelectable = false
+        
       //  self.textViewMultiline.textContainer.lineBreakMode = .byTruncatingTail
      //   self.textViewMultiline.textContainer.heightTracksTextView = true
         
@@ -33,26 +53,73 @@ class ViewController: UIViewController {
         self.labelMultiline.numberOfLines = 0
         self.labelMultiline.lineBreakMode = .byTruncatingTail
         
-        self.setMultiLineTextWithNewLinesAndLink()
+        
+        let textviewTap = UITapGestureRecognizer(target: self, action: #selector(self.textViewTapped))
+        textviewTap.numberOfTapsRequired = 1
+        self.textViewMultiline.addGestureRecognizer(textviewTap)
+        
+       // self.setMultiLineTextWithLink()
+        
+    }
+    
+    
+    func textViewTapped(){
+        
+        if self.textViewHeightConstraint.constant == 58{
+            self.textViewMultiline.isShrink = false
+            self.textViewHeightConstraint.constant = 300
+           // self.textViewMultiline.layoutIfNeeded()
+        }
+        else{
+            self.textViewMultiline.isShrink = true
+            self.textViewHeightConstraint.constant = 58
+           // self.textViewMultiline.layoutIfNeeded()
+        }
+        
+        switch selctedTextType {
+        case 0:
+            self.setMultiLineText()
+        case 1:
+            self.setMultilineTextA()
+        case 2:
+            self.setMultiLineTextWithNewLinesAndLink()
+        default:
+            self.setMultiLineTextWithNewLinesAndLink()
+        }
         
     }
     
     func setMultiLineText(){
-        self.textViewMultiline.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        selctedTextType = 0
+        self.textViewMultiline.isTextModified = false
+        self.textViewMultiline.text = "Lorem ipsum dolor sit er \nLorem ipsum dolor sit er \n cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         
         self.labelMultiline.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
     }
     
     func setMultiLineTextWithLink(){
+        selctedTextType = 1
+        self.textViewMultiline.isTextModified = false
         self.textViewMultiline.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. http://google.com . Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         
         self.labelMultiline.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. http://google.com . Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
     }
     
     func setMultiLineTextWithNewLinesAndLink(){
-        self.textViewMultiline.text = "jdjdjsjskkskdk\ndmdkdk\nhttp://g@b.com"
+        selctedTextType = 2
+        self.textViewMultiline.isTextModified = false
+        self.textViewMultiline.text = "jdjdjsjskkskdkdkd\n\ndmdkdk\nhttp://www.g@b.com"
         
         self.labelMultiline.text = "jdjdjsjskkskdkdkd\n\ndmdkdk\nhttp://g@b.com"
+    }
+
+    
+    func setMultilineTextA(){
+        selctedTextType = 1
+        self.textViewMultiline.isTextModified = false
+        self.textViewMultiline.text = "gxncjd sfkdgjstj afjdhkfy dgjstjt c xahg🤕🤮🤧🤕🤮🤕😺🙈💩😸😺😸👽jfjfjgkgkgk gmqhktktj\nfjjdfnnffj\nfnfncncn\nfjjfnffn\nfmnfncfm"
+        
+        self.labelMultiline.text = "gxncjd sfkdgjstj afjdhkfy dgjstjt c xahg🤕🤮🤧🤕🤮🤕😺🙈💩😸😺😸👽jfjfjgkgkgk gmqhktktj\nfjjdfnnffj\nfnfncncn\nfjjfnffn\nfmnfncfm"
     }
     
     
@@ -60,7 +127,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
